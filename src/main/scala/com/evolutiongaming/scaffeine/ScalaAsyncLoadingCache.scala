a955fa2fe32fb341eeeb0a530c6eb7124ed433c3
Sergey Tavanets <stavanets@evolutiongaming.com>
2017-04-04 14:58:11 +0200

    removed a requirement to provide an implicit execution context in ScalaAsyncLoadingCache.getAsync method

--- src/main/scala/com/evolutiongaming/scaffeine/ScalaAsyncLoadingCache.scala
+++ src/main/scala/com/evolutiongaming/scaffeine/ScalaAsyncLoadingCache.scala
@@ -7,7 +7,7 @@ import scala.compat.java8.FutureConverters._
 import scala.concurrent.{ExecutionContext, Future}
 
 class ScalaAsyncLoadingCache[K, V](override val underlying: CaffeineAsyncLoadingCache[K, V])(implicit ec: ExecutionContext) extends AsyncLoadingCache(underlying) {
-  def getAsync(key: K)(implicit ec: ExecutionContext): Future[Option[V]] = underlying.get(key).toScala.map(Option(_))
+  def getAsync(key: K): Future[Option[V]] = underlying.get(key).toScala.map(Option(_))
 
   def getSync(key: K): Option[V] = Option(underlying.synchronous().get(key))
 }
