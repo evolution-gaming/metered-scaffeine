0898e7baa9ee7b226114c14d349fa620acdc61b9
Sergey Tavanets <stavanets@evolutiongaming.com>
2017-05-11 18:24:05 +0200

    optimised asynchronous getter to be more efficient for already completed futures

--- src/main/scala/com/evolutiongaming/scaffeine/ScalaAsyncLoadingCache.scala
+++ src/main/scala/com/evolutiongaming/scaffeine/ScalaAsyncLoadingCache.scala
@@ -9,7 +9,12 @@ import scala.concurrent.{ExecutionContext, Future}
 class ScalaAsyncLoadingCache[K, V](override val underlying: CaffeineAsyncLoadingCache[K, V])
   (implicit ec: ExecutionContext) extends AsyncLoadingCache(underlying) {
 
-  def getAsync(key: K): Future[Option[V]] = underlying.get(key).toScala.map(Option(_))
+  def getAsync(key: K): Future[Option[V]] = {
+    (underlying.get(key) match {
+      case f if f.isDone && !f.isCompletedExceptionally && !f.isCancelled => Future.successful(f.get())
+      case f => f.toScala
+    }).map(Option(_))
+  }
 
   def getSync(key: K): Option[V] = Option(underlying.synchronous().get(key))
 }
